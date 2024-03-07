ce54391f7bf6188c511a5142ceab3605ecccc622
keynesabid <keynesabid@gmail.com>
2024-03-07 21:28:23 +0800

    Add style for Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { Link, Navigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import UserContext from "../UserContext";
+import Container from 'react-bootstrap/Container'; 
 
 export default function Login() {
 
@@ -112,41 +113,47 @@ export default function Login() {
 	    	<Navigate to="/products" />
 	    :
 	    	<>
-		    <Form onSubmit={(e) => authenticate(e)}>
-		    	<h1 className="my-5 text-center">Login</h1>
-		        <Form.Group controlId="userEmail">
-		            <Form.Label>Email address</Form.Label>
-		            <Form.Control 
-		                type="email" 
-		                placeholder="Enter email"
-		                value={email}
-		    			onChange={(e) => setEmail(e.target.value)}
-		                required
-		            />
-		        </Form.Group>
+			<div className='back-login'>
+				<Container className='login-cont rounded w-25 p-3 mt-5'>
+					<Form onSubmit={(e) => authenticate(e)}>
+						<h1 className="mb-3 text-center">Login</h1>
+						<Form.Group controlId="userEmail">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control 
+								type="email" 
+								placeholder="Enter Email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</Form.Group>
 
-		        <Form.Group controlId="password">
-		            <Form.Label>Password</Form.Label>
-		            <Form.Control 
-		                type="password" 
-		                placeholder="Password"
-		                value={password}
-		    			onChange={(e) => setPassword(e.target.value)}
-		                required
-		            />
-		        </Form.Group>
-
-		        { isActive ? 
-		            <Button variant="primary" type="submit" id="submitBtn">
-		                Submit
-		            </Button>
-		            : 
-		            <Button variant="danger" type="submit" id="submitBtn" disabled>
-		                Submit
-		            </Button>
-		        }
-		    </Form>
-		    <h6>Dont have an account? <Link to={"/register"}>Register</Link></h6>
+						<Form.Group controlId="password">
+							<Form.Label className='py-1'>Password</Form.Label>
+							<Form.Control 
+								type="password" 
+								placeholder="Enter Password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</Form.Group>
+					
+						{ isActive ? 
+						
+							<Button className=" float-end submitButton my-3" variant="primary" type="submit" id="submitBtn">
+								Submit
+							</Button>
+							: 
+							<Button className="submitButton my-3 float-end" variant="danger" type="submit" id="submitBtn" disabled>
+								Submit
+							</Button>
+						}
+					</Form>
+					<h6 className='question'>Dont have an account? <Link className='register' to={"/register"}>Register</Link></h6>
+				</Container>
+				
+			</div>
 		    </>  
     )
 }
