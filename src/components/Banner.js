bf762ea70efa20949ebe1022e1ffb0d8e3b7ce49
adryanbabaran <adryanbabaran@gmail.com>
2024-03-11 19:22:21 +0800

    Add banner template

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,13 @@
 import { Button, Row, Col } from "react-bootstrap";
 import { Link } from 'react-router-dom';
-import HazeLogo from '../images/HAZE-logo.png'; // Import the image
+import HazeLogo from '../images/HAZE.png'; // Import the image
 
 export default function Banner() {
     return (
         <Row>
             <Col>
-                <Link to="/products">
-                    <img src={HazeLogo} alt="HAZE" />
+                <Link to="/">
+                    <img src={HazeLogo} alt="HAZE" /> {/* Use the imported image */}
                 </Link>
             </Col>
         </Row>
