0482954c6d28a620b070955fd70619003cd47e2d
adryanbabaran <adryanbabaran@gmail.com>
2024-03-11 19:38:32 +0800

    Add banner template

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import HazeLogo from '../images/HAZE.png'; // Import the image
 export default function Banner() {
     return (
         <Row>
-            <Col>
+            <Col className="d-flex justify-content-center">
                 <Link to="/">
                     <img src={HazeLogo} alt="HAZE" /> {/* Use the imported image */}
                 </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import Banner from "../components/Banner";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 export default function Home() {
 
@@ -7,7 +8,7 @@ export default function Home() {
             <Banner />
             <h1 className='mt-3'>Welcome to <strong className="head-name">Haze</strong>!</h1>
             <p>Haze includes a digital storefront called the Haze Store through which users can purchase games and game merchandise.</p>
-            
+            <FeaturedProducts />
         </>
     )
 }
